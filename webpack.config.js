0c555765bae364cb4799324d5def9333d63553b5
CALE A. NEWMAN <canewman@us.ibm.com>
2015-03-10 18:17:11 -0500

    changed start script and added react hot loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,27 @@
+var webpack = require('webpack');
+
 module.exports = {
   devtool: 'eval',
   
-  entry: './app.js',
+  entry: [
+    'webpack-dev-server/client?http://0.0.0.0:3000',
+    'webpack/hot/only-dev-server',
+    './app.js'
+  ],
   output: {
     filename: 'bundle.js',
     path: __dirname
   },
-  
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.css']
+  },
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'babel!jsx-loader', exclude: /node_modules/ },
+      { test: /\.jsx?$/, loaders: ['babel', 'react-hot', 'jsx-loader'], exclude: /node_modules/ },
       { test: /\.css$/, loader: 'style!css', exclude: /node_modules/ }
     ]
   }
