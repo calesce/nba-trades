2329fccc1ea34a9269fd5d7b7ffb2700b3cb0e73
CALE A. NEWMAN <canewman@us.ibm.com>
2015-06-19 20:34:15 -0500

    prod blah pt 4

--- server.js
+++ server.js
@@ -1,25 +1,10 @@
 var webpack = require('webpack');
-
 var express = require('express');
 var salaries = require('./lib/salaries');
 var url = require('url');
 var path = require('path');
 
-var app = express();
-
-var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Origin, Accept');
-  next();
-};
-
-app.use(allowCrossDomain);
-
 var isProduction = process.env.NODE_ENV === 'production';
-
-app.get('/api', allowCrossDomain, salaries);
 
 if(isProduction) {
   var config = require('./webpack.production');
@@ -27,9 +12,25 @@ if(isProduction) {
 
   server.use(express.static(path.join(__dirname, '/dist')));
 
+  server.get('/api', salaries);
+
   server.listen(3000);
 }
 else {
+  var app = express();
+
+  var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Origin, Accept');
+    next();
+  };
+
+  app.use(allowCrossDomain);
+
+  app.get('/api', allowCrossDomain, salaries);
+
   var config = require('./webpack.config');
   var WebpackDevServer = require('webpack-dev-server');
 
