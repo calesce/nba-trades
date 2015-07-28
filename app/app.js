65adb4bbb8171946159040bbcec771fa35034ad0
Cale Newman <newman.cale@gmail.com>
2015-07-27 22:53:36 -0500

    fixed hot reloading for stores

--- app/app.js
+++ app/app.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import App from './containers/App';
 
-import { createStore, combineReducers } from 'redux';
-import * as reducers from './reducers';
-
-import * as TradeActions from './actions/TradeActions';
-import * as types from './constants/ActionTypes';
-
 (function initialize() {
 
   fetch('http://localhost:8080/api')
@@ -14,16 +8,8 @@ import * as types from './constants/ActionTypes';
       return response.json();
     })
     .then((teams) => {
-      const reducer = combineReducers(reducers);
-      const store = createStore(reducer);
-
-      store.dispatch({
-        type: types.FETCH_NBA_DATA,
-        teams
-      });
-
       return React.render(
-        <App store={store} />,
+        <App teams={teams} />,
         document.getElementById('trades')
       );
     })
--- app/containers/App.js
+++ app/containers/App.js
@@ -3,10 +3,24 @@ import { Provider } from 'react-redux';
 
 import TradeMachine from '../components/TradeMachine';
 
+import { createStore, combineReducers } from 'redux';
+import * as reducers from '../reducers';
+import * as types from '../constants/ActionTypes';
+
+const reducer = combineReducers(reducers);
+const store = createStore(reducer);
+
 export default class App extends Component {
+  componentDidMount() {
+    store.dispatch({
+      type: types.FETCH_NBA_DATA,
+      teams: this.props.teams
+    });
+  }
+
   render() {
     return (
-      <Provider store={this.props.store}>
+      <Provider store={store}>
         {() => <TradeMachine /> }
       </Provider>
     );
